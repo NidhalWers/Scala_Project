99b17d0fb716c36a7aae52ee9f8dc47cacd764d1
NidhalWers <nteyeb75@gmail.com>
2021-11-05 16:11:08 +0100

    all case classes verified

--- src/main/scala/models/Countries.scala
+++ src/main/scala/models/Countries.scala
@@ -11,12 +11,12 @@ object Countries{
   def apply(line : String): Countries = {
     val data = line.split("," )
     new Countries(
-      data(0)
-      data(1)
-      data(2)
-      data(3)
-      data(4)
-      data(5)
+      data(0).toInt,
+      data(1),
+      data(2),
+      data(3),
+      data(4),
+      data(5),
     )
   }
 }
--- src/main/scala/models/Runways.scala
+++ src/main/scala/models/Runways.scala
@@ -1,9 +1,8 @@
 package models
 
-import javax.swing.text.html.Option
-
 case class Runways(id : Int,
                    airportRef : Int,
+                   airportIdent: String,
                    lengthFt : String,
                    widthFt : Int,
                    surface : String,
@@ -23,28 +22,29 @@ case class Runways(id : Int,
                    heDisplacedThresholdFt :Int)
 
 object Runways {
-  def apply(line : String): Runways{
+  def apply(line : String): Runways = {
     val data = line.split(",")
-    new Runways{
+    new Runways(
       data(0).toInt,
-      data(0).toInt,
-      data(0).toString,
-      data(0).toInt,
-      data(0).toString,
-      data(0).toBoolean,
-      data(0).toBoolean,
-      data(0).toString,
-      data(0).toFloat,
-      data(0).toFloat,
-      data(0).toInt,
-      data(0).toInt,
-      data(0).toInt,
-      data(0).toString,
-      data(0).toFloat,
-      data(0).toFloat,
-      data(0).toInt,
-      data(0).toInt,
-      data(0).toInt,
-    }
+      data(1).toInt,
+      data(2),
+      data(3),
+      data(4).toInt,
+      data(5),
+      data(6).toBoolean,
+      data(7).toBoolean,
+      data(8),
+      data(9).toFloat,
+      data(10).toFloat,
+      data(11).toInt,
+      data(12).toInt,
+      data(13).toInt,
+      data(14),
+      data(15).toFloat,
+      data(16).toFloat,
+      data(17).toInt,
+      data(18).toInt,
+      data(19).toInt
+    )
   }
 }
