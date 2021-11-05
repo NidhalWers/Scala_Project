a8c2bda0c6d3922f6a23c82d96e25887cb7f7a61
fares0211 <faresamazigj7848@gmail.com>
2021-11-05 16:03:52 +0100

    case class Countries

--- src/main/scala/models/Countries.scala
+++ src/main/scala/models/Countries.scala
@@ -1,16 +1,24 @@
 package models
 
-case class Countries(id : Int, code : String, name : String, continent : String, wikipediaLink : String, keywords : String )
+case class Countries(id : Int,
+                     code : String,
+                     name : String,
+                     continent : String,
+                     wikipediaLink : String,
+                     keywords : String )
 
-case class CountriesBuilder (id : Option[Int], code : Option[String], name : Option[String], continent : Option[String], wikipediaLink : Option[String], keywords : Option[String] ) {
-  def id(id : Int) = this.copy(id= Some(id))
-  def code(code : String) = this.copy(code= Some(code))
-  def name(name : String) = this.copy(name= Some(name))
-  def continent(continent : String) = this.copy(continent= Some(continent))
-  def wikipedia_link(wikipediaLink : String) = this.copy(wikipediaLink= Some(wikipediaLink))
-  def keywords(keywords : String) = this.copy(keywords= Some(keywords))
+object Countries{
+  def apply(line : String): Countries = {
+    val data = line.split("," )
+    new Countries(
+      data(0)
+      data(1)
+      data(2)
+      data(3)
+      data(4)
+      data(5)
+    )
+  }
 }
-
-
 
 
