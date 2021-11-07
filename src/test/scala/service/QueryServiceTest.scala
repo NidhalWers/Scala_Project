8dbe130e2cf568ac8e5d2b0720f9286995909bd0
NidhalWers <nteyeb75@gmail.com>
2021-11-07 17:51:39 +0100

    preparation for unit tests [unit tests]

---
+++ src/test/scala/service/QueryServiceTest.scala
@@ -0,0 +1,25 @@
+package service
+
+class QueryServiceTest extends org.scalatest.FunSuite {
+
+  test("testAirportService") {
+
+  }
+
+  test("testQueryName") {
+
+  }
+
+  test("testCountryService") {
+
+  }
+
+  test("testQueryCode") {
+
+  }
+
+  test("testRunwayService") {
+
+  }
+
+}
---
+++ src/test/scala/service/UtilsTest.scala
@@ -0,0 +1,21 @@
+package service
+
+class UtilsTest extends org.scalatest.FunSuite {
+
+  test("testMyToBoolean") {
+
+  }
+
+  test("testMyToString") {
+
+  }
+
+  test("testMyToFloat") {
+
+  }
+
+  test("testMyToInt") {
+
+  }
+
+}
