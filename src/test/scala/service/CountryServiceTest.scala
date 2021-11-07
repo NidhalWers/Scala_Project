dbbc8c67cfc023da457aeb6f10c5616d2dadbfe5
fares0211 <faresamazigj7848@gmail.com>
2021-11-07 19:26:38 +0100

    UtilsTest + CountryServiceTest

--- src/test/scala/service/CountryServiceTest.scala
+++ src/test/scala/service/CountryServiceTest.scala
@@ -1,11 +1,15 @@
 package service
 
+
 class CountryServiceTest extends org.scalatest.FunSuite {
+  val countryService = new CountryService
 
   test("testGetCodeFromName") {
 
   }
 
+  // TODO créer un fichier .csv avec 1 seule ligne. Passer le fichier en paramètre et tester les valeurs (comme
+  // TODO CountryTest
   test("testConvertFileToCountries") {
 
   }
