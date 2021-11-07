62e696524bfecba62bc6b6604140c9463e388128
NidhalWers <nteyeb75@gmail.com>
2021-11-07 10:58:15 +0100

    report : 10 highest number / 10 lowest number [tested] [no unit tests]

--- src/main/scala/controller/ConsoleController.scala
+++ src/main/scala/controller/ConsoleController.scala
@@ -1,19 +1,26 @@
 package controller
 
 import models.{Airport, Country, Runway}
-import service.{CountryService, QueryService}
+import service.{CountryService, QueryService, ReportService}
+
 
 class ConsoleController {
 
   val countryService = new CountryService
   val queryService = new QueryService
+  val reportService = new ReportService
+
 
   def queryAndReports( airports: List[Airport], countries: List[Country], runways: List[Runway] ): Unit = {
-    println("Please choose between option 1 & 2 :\n\t1 - Query\n\t2 - Reports")
-    val input = scala.io.StdIn.readInt()
+    println("Please choose between an option :" +
+      "\n\t1 - Query" +
+      "\n\t2 - Reports" +
+      "\n\t3 - Quit")
+    val input = scala.io.StdIn.readLine()
     input match {
-      case 1 => query( airports, countries, runways )
-      case 2 => report( airports, countries, runways )
+      case "1" => query( airports, countries, runways )
+      case "2" => report( airports, countries, runways )
+      case "3" => println("good by")
       case _ => queryAndReports( airports, countries, runways )
     }
   }
@@ -24,14 +31,55 @@ class ConsoleController {
     if ( countryService.isStringCountryCode(input) ){
       val result = queryService.queryCode("\""+input+"\"", airports, runways)
       println(result.map{case (k,v) => k + " : "+ v }.mkString("\n")  )
+
+      queryAndReports(airports,countries,runways)
     }else{
       val result = queryService.queryName("\""+input+"\"", countries, airports, runways)
       println(result)
+
+      queryAndReports(airports,countries,runways)
     }
   }
 
-  private def report( airports: List[Airport], countries: List[Country], runways: List[Runway] ): Unit ={
-    println("This is a menu")
+
+
+    private def report(airports: List[Airport], countries: List[Country], runways: List[Runway] ): Unit ={
+    println("This is a menu, choose the informations you want :" +
+      "\n\t1 - 10 countries with highest number of airports" +
+      "\n\t2 - 10 countries with lowest number of airports " +
+      "\n\t3 - Type of runways per country" +
+      "\n\t4 - The top 10 most common runway latitude")
+
+    val input = scala.io.StdIn.readLine()
+    input match {
+      case "1" => {
+        val result = reportService.getTenCountriesHighestNumberOfAirports(airports)
+        println(result)
+
+        queryAndReports(airports,countries,runways)
+      }
+      case "2" => {
+        val result = reportService.getTenCountriesLowestNumberOfAirports(airports)
+        println(result)
+
+        queryAndReports(airports,countries,runways)
+      }
+      case "3" => {
+        reportService.getTypeOfRunwaysPerCountry(airports, runways)
+
+        queryAndReports(airports,countries,runways)
+      }
+      case "4" => {
+        reportService.getTenMostCommonRunwaysLatitude(runways)
+
+        queryAndReports(airports,countries,runways)
+      }
+      case "5" => {
+        report(airports, countries, runways)
+
+        queryAndReports(airports,countries,runways)
+      }
+    }
   }
 
 
--- src/main/scala/service/ReportService.scala
+++ src/main/scala/service/ReportService.scala
@@ -1,5 +1,50 @@
 package service
 
+import models.{Airport, Runway}
+
+import scala.collection.immutable.ListMap
+
 class ReportService {
 
+  def getTenCountriesHighestNumberOfAirports(airports : List[Airport]): Map[String, Int] = {
+    val mapOfNumber = createMapOfNumberAirports(airports)
+
+    ListMap(mapOfNumber.toSeq.sortWith(_._2 > _._2):_*).take(10)
+  }
+
+
+
+  def getTenCountriesLowestNumberOfAirports(airports : List[Airport]): Map[String, Int] = {
+    val mapOfNumber = createMapOfNumberAirports(airports)
+
+    ListMap(mapOfNumber.toSeq.sortWith(_._2 < _._2):_*).take(10)
+  }
+
+
+  private def createMapOfNumberAirports(airports : List[Airport]): Map[String, Int] = {
+    def aux(airports : List[Airport], acc: Map[String, Int] ): Map[String, Int] = airports match {
+      case Nil => acc
+      case x :: xs =>
+        val key = x.isoCountry.get
+        if(acc.contains(key)){
+          val nb = acc(key)
+          val acc2 = acc.-(key)
+          aux( xs, acc2 + (key -> (nb+1)) )
+        }else{
+          aux( xs, acc + (key -> (1)) )
+        }
+    }
+
+    aux(airports, Map[String,Int]())
+
+  }
+
+
+  def getTypeOfRunwaysPerCountry(airports : List[Airport], runways: List[Runway]): Map[ String,List[String] ] = {
+    null
+  }
+
+  def getTenMostCommonRunwaysLatitude(runways: List[Runway]): List[Float] = {
+    null
+  }
 }
