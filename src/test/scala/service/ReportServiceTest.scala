c97f0820a392cc7a19035f1d1a950417e320d9c3
Mohamed <z.mohamedpro@gmail.com>
2021-11-08 20:57:04 +0100

    Runways structure3

--- src/test/scala/service/ReportServiceTest.scala
+++ src/test/scala/service/ReportServiceTest.scala
@@ -1,6 +1,14 @@
 package service
 
+
+
 class ReportServiceTest extends org.scalatest.FunSuite {
+
+  val airports = airportService.convertFileToAirports("resources/airports.csv")
+
+  val countries = countryService.convertFileToCountries("resources/countries.csv")
+
+  val runways = runwayService.convertFileToRunways("resources/runways.csv")
 
   test("testGetTenMostCommonRunwaysLatitude") {
 
